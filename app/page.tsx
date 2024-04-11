3c4f6a2847b152cdccea528ff7c2f1f5821a288c
KDavis006 <110576696+KDavis006@users.noreply.github.com>
2024-04-11 15:47:18 -0700

    navbar

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Nav from "./Components/Navbar/Nav";
+
+export default function Home() {
+  return (
+    <header className="bg-black shadow">
+      <Nav />
+    </header>
+  );
+}
--- server/middleware/auth.js
+++
@@ -1,6 +0,0 @@
-function isAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) return next();
-  return res.redirect('/');
-}
-
-module.exports = isAuthenticated;
